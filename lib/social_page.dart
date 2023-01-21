049c3a030b920d829e33a1736f8f7b4996ea29ee
Therocketguy1204 <sachitshastri@gmail.com>
2023-01-21 14:21:40 +0530

    Containers update

--- lib/social_page.dart
+++ lib/social_page.dart
@@ -31,22 +31,53 @@ class _SocialsPageState extends State<SocialsPage> {
               child: Row(
                 // ignore: prefer_const_literals_to_create_immutables
                 children: [
-                  const Text('FirstName'),
+                  const Text(' FirstName'),
                   const Text(' '),
-                  const Text('LastName'),
+                  const Text(' LastName'),
                 ],
               ), //Firstname and Lastname
-            ),
+            ), //Name container
             Row(
               children: const [Text(' ')],
             ), //empty row
 
+            Container(
+              height: 50,
+              width: 200,
+              decoration: BoxDecoration(
+                  border: Border.all(
+                color: Colors.black,
+                width: 2,
+              )),
+              child: Row(
+                children: const [
+                  // ignore: prefer_const_constructors
+                  Text(' Location'),
+                ],
+              ),
+            ),
+
             Row(
-              children: const [
-                // ignore: prefer_const_constructors
-                Text('Location'),
-              ],
-            ), //Location
+              children: const [Text(' ')],
+            ), //empty row
+
+            Container(
+              height: 50,
+              width: 200,
+              decoration: BoxDecoration(
+                  border: Border.all(
+                color: Colors.black,
+                width: 2,
+              )),
+              child: Row(
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  const Text(' 10 following'),
+                  const Text(' '),
+                  const Text(' 20 followers'),
+                ],
+              ),
+            ),
           ],
         ));
   }
