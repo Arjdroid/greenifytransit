852c68d049d712e85dfc63aca87e176e6de3c866
Therocketguy1204 <sachitshastri@gmail.com>
2023-01-21 16:25:29 +0530

    added friends list

--- lib/social_page.dart
+++ lib/social_page.dart
@@ -18,114 +18,125 @@ class _SocialsPageState extends State<SocialsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Socials page'),
+      appBar: AppBar(
+        title: const Text('Socials page'),
+      ),
+      body: Column(children: [
+        Row(
+          children: const [Text(' ')],
+        ), //Empty row
+
+        Container(
+          // margin: const EdgeInsets.fromLTRB(50, 50, 50, 50),
+          height: 50,
+          width: 200,
+          color: Colors.blueGrey,
+
+          child: Row(
+            // ignore: prefer_const_literals_to_create_immutables
+
+            children: const [
+              Text(
+                ' FirstName',
+                style: TextStyle(color: Colors.white),
+              ),
+              Text(' '),
+              Text(
+                ' LastName',
+                style: TextStyle(color: Colors.white),
+              ),
+            ],
+          ), //Firstname and Lastname
+        ), //Name container
+        Row(
+          children: const [Text(' ')],
+        ), //empty row
+
+        Container(
+          height: 50,
+          width: 200,
+          color: Colors.blueGrey,
+          child: Row(
+            children: const [
+              // ignore: prefer_const_constructors
+              Text(
+                ' Location - City, State, Country',
+                style: TextStyle(color: Colors.white),
+              ),
+            ],
+          ),
         ),
-        body: Column(
-          children: [
-            Row(
-              children: const [Text(' ')],
-            ), //Empty row
 
-            Container(
-              height: 50,
-              width: 200,
-              color: Colors.blueGrey,
+        Row(
+          children: const [Text(' ')],
+        ), //empty row
 
-              child: Row(
-                // ignore: prefer_const_literals_to_create_immutables
-
-                children: const [
-                  Text(
-                    ' FirstName',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  Text(' '),
-                  Text(
-                    ' LastName',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ],
-              ), //Firstname and Lastname
-            ), //Name container
-            Row(
-              children: const [Text(' ')],
-            ), //empty row
-
-            Container(
-              height: 50,
-              width: 200,
-              color: Colors.blueGrey,
-              child: Row(
-                children: const [
-                  // ignore: prefer_const_constructors
-                  Text(
-                    ' Location - City, State, Country',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ],
+        Container(
+          height: 50,
+          width: 200,
+          color: Colors.blueGrey,
+          child: Row(
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              const Text(
+                ' 10 following',
+                style: TextStyle(color: Colors.white),
               ),
-            ),
-
-            Row(
-              children: const [Text(' ')],
-            ), //empty row
-
-            Container(
-              height: 50,
-              width: 200,
-              color: Colors.blueGrey,
-              child: Row(
-                // ignore: prefer_const_literals_to_create_immutables
-                children: [
-                  const Text(
-                    ' 10 following',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  const Text(
-                    ' -- ',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  const Text(
-                    '20 followers',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ],
+              const Text(
+                ' -- ',
+                style: TextStyle(color: Colors.white),
               ),
-            ),
-
-            Row(
-              children: const [Text(' ')],
-            ), //empty row
-
-            Row(
-              children: const [Text(' Statistics: ')],
-            ),
-
-            Row(
-              children: const [Text(' ')],
-            ), //empty row
-
-            Container(
-              height: 200,
-              width: 200,
-              child: SfCartesianChart(
-                title: ChartTitle(text: 'carbon emssions saved this month'),
-                series: <ChartSeries>[
-                  LineSeries<CarbonEmsissionData, int>(
-                      pointColorMapper:
-                          (CarbonEmsissionData carbonEmissions, _) =>
-                              carbonEmissions.color,
-                      dataSource: chartData,
-                      xValueMapper: (CarbonEmsissionData carbonEmissions, _) =>
-                          carbonEmissions.weeks,
-                      yValueMapper: (CarbonEmsissionData carbonEmissions, _) =>
-                          carbonEmissions.carbonEmisions)
-                ],
+              const Text(
+                '20 followers',
+                style: TextStyle(color: Colors.white),
               ),
-            ),
+            ],
+          ),
+        ),
+
+        Row(
+          children: const [Text(' ')],
+        ), //empty row
+
+        Row(
+          children: const [Text(' Statistics: ')],
+        ),
+
+        Row(
+          children: const [Text(' ')],
+        ), //empty row
+
+        Container(
+          height: 200,
+          width: 200,
+          child: SfCartesianChart(
+            title: ChartTitle(text: 'carbon emssions saved this month'),
+            series: <ChartSeries>[
+              LineSeries<CarbonEmsissionData, int>(
+                  pointColorMapper: (CarbonEmsissionData carbonEmissions, _) =>
+                      carbonEmissions.color,
+                  dataSource: chartData,
+                  xValueMapper: (CarbonEmsissionData carbonEmissions, _) =>
+                      carbonEmissions.weeks,
+                  yValueMapper: (CarbonEmsissionData carbonEmissions, _) =>
+                      carbonEmissions.carbonEmisions)
+            ],
+          ),
+        ),
+
+        Row(
+          children: const [Text(' ')],
+        ), //empty row
+
+        Row(
+          children: const [
+            Text(' Friends: ', style: TextStyle(color: Colors.deepOrange))
           ],
-        ));
+        ),
+
+        IconButton(icon: Icon(Icons.account_circle), onPressed: () => {})
+      ]),
+    );
   }
 }
 
