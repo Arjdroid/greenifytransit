afc6191930473395578cc3971b39a9a9f5393bd4
Arjun Singh <57623600+Arjdroid@users.noreply.github.com>
2023-01-21 11:28:54 +0530

    Laying the groundwork

--- lib/routes_planner.dart
+++ lib/routes_planner.dart
@@ -1,1 +1,18 @@
 // Starting the Routes Planner Stuff
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class RoutesPlanner extends StatefulWidget {
+  const RoutesPlanner({super.key});
+
+  @override
+  State<RoutesPlanner> createState() => _RoutesPlannerState();
+}
+
+class _RoutesPlannerState extends State<RoutesPlanner> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
